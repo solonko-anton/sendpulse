1ebfd9ec61fe65b8457ce4f2d8ea08602ce1c284
solonko-anton <asolonko42@gmail.com>
2025-07-24 15:12:19 +0000

    Extend task time to 24 hours

--- app/main.py
+++ app/main.py
@@ -64,7 +64,7 @@ async def subscribe(
             )
         )
 
-        schedule_email_task.apply_async(args=[email, file_path, plan], countdown=4)
+        schedule_email_task.apply_async(args=[email, file_path, plan], countdown=24*3600)
 
         return {
             "message": f"Email на {email} надіслано.",
